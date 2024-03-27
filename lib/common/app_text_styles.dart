52031fa7bdfe412c8cb2a150473df3849058a6ba
juan98buri <juan98buri@gmail.com>
2024-03-27 15:53:55 -0500

    food pizza

--- lib/common/app_text_styles.dart
+++ lib/common/app_text_styles.dart
@@ -2,19 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyle {
-  /*static TextStyle getAppTextStyle(
-		{double size = 14,
-		FontWeight weight = FontWeight.normal,
-		FontStyle style = FontStyle.normal,
-		Color color = Colors.black}) {
-	  return TextStyle(
-		  color: color,
-		  fontSize: size,
-		  fontWeight: weight,
-		  fontStyle: style,
-		  fontFamily: "Jost"
-	}*/
-
   static TextStyle titleItemStyle = GoogleFonts.openSans(
     fontWeight: FontWeight.w700,
     fontSize: 16,
